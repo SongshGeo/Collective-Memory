6844edd2e641d10957e1e4906e52a5f99d843b69
SongshGeo <songshgeo@gmail.com>
2020-04-03 10:15:47 +0800

    update get parameters

--- collective_memory.py
+++ collective_memory.py
@@ -8,7 +8,7 @@ Beijing Normal University
 """
 
 from simulating_floods import generate_random_flood_series
-from data_process import actual_population, get_last_k, get_last_pqr, get_actual_levee_height
+from data_process import actual_population, get_last_k, get_last_pqr, get_actual_levee_height, get_last_mius
 
 import pandas as pd
 import numpy as np
@@ -20,7 +20,6 @@ P_MAX: float = 200 * 10000 * 2.5  # positive estimation of the population limits
 THRESHOLD_F = 0  # The threshold for enhance.
 START_YEAR = 1940  # The start year of this study.
 INITIAL_POP = actual_population[START_YEAR]  # Population in the start year (1940).
-P, R, Q = [get_last_pqr()[k] for k in ['p', 'r', 'q']]  # Fitted parameters of integrate model
 K_OSM, K_IUDM = [get_last_k()[k] for k in ['osm', 'iudm']]  # Use the last result of optimise
 
 """ Global Parameters """
@@ -45,9 +44,6 @@ RHO_D = 0.00935  # estimated by fitting
 # According to (Di Baldassarre et al. 2017)
 ETA_H = 1.1  # Safety factor for raising levees
 
-# According to (Di Baldassarre et al. 2017)
-MIU_S = 0.04  # Decay rate of collective memory, from the osm
-
 
 def get_initial_values():
     """
@@ -64,37 +60,44 @@ def get_initial_values():
     return initial_values
 
 
-def osm(state, steps, k_osm):
+def osm(state, steps, k_osm, how='exp'):
+    miu_s = get_last_mius()  # Decay rate of collective memory, from the osm
     f, m, d = state
-    delta_m = - MIU_S * m + f * d
-    delat_d = d * (k_osm - ALPHA_D * m)
+    delta_m = - miu_s * m + f * d
+    if how == 'exp':
+        delta_d = d * (k_osm - ALPHA_D * m)
+    else:
+        delta_d = k_osm * (1 - d * (1 + ALPHA_D * m)) - f * d
     new_m = m + delta_m * steps
-    new_d = d + delat_d * steps
+    new_d = d + delta_d * steps
     return [new_m, new_d]
 
 
-def iudm(state, steps, k_iudm):
+def iudm(state, steps, k_iudm, how='exp'):
+    p, r, q = [get_last_pqr()[k] for k in ['p', 'r', 'q']]
     f, u, v, d = state
     m = u + v
-    delta_u = -(P + R) * u + f * d
-    delta_v = -Q * v + R * u
-    delta_d = d * (k_iudm - ALPHA_D * m)
+    delta_u = -(p + r) * u + f * d
+    delta_v = -q * v + r * u
+    del_d = d * (k_iudm - ALPHA_D * m) if how == 'exp' else k_iudm * (1 - d * (1 + ALPHA_D * m)) - f * d
     new_u = u + delta_u * steps
     new_v = v + delta_v * steps
-    new_d = d + delta_d * steps
+    new_d = d + del_d * steps
     return [new_u, new_v, new_d]
 
 
 def get_levee_height(state, how):
     y, w, u, v, m, d_osm, d_iudm, h, f = state
     if how == 'enhance':  # If we can heighten levee after floodings.
-        if f >= THRESHOLD_F:
+        if w > h:
             r = ETA_H * (w + XI_H - h)
             h = r + h
         else:
             pass
     elif how == 'fixed':
         h = get_actual_levee_height(y)
+    elif how is None:
+        h = 0
     return h
         
 
@@ -106,19 +109,38 @@ def judge_damage(w, h):
     return f
 
 
-def main_function(flood_ser, steps, how='fixed', k_osm=K_OSM, k_iudm=K_IUDM):
+def print_parameters(title=False):
+    miu_s = get_last_mius()  # Decay rate of collective memory, from the osm
+    p, r, q = [get_last_pqr()[k] for k in ['p', 'r', 'q']]
+    if title:
+        return "p = {:.3f}, r = {:.3f}, q = {:.3f}, miu_s = {:.3f}".format(p, r, q, miu_s)
+    else:
+        print("Used params in this simulation:")
+        print("p = {:.3f}, r = {:.3f}, q = {:.3f}".format(p, r, q))
+        print("miu_s = {:.3f}".format(miu_s))
+
+
+def main_function(flood_ser, steps=1, how='fixed', pop_growth='exp', k_osm=K_OSM, k_iudm=K_IUDM, print_params=True):
+    if print_params:
+        print_parameters()
+        print("k_osm = {:.3f}, k_iudm = {:.3f}".format(k_osm, k_iudm))
     y, w, u, v, m, d_osm, d_iudm, h, f = get_initial_values()
+    if how != 'fixed':
+        h = 0
     df = pd.DataFrame(index=flood_ser.index, columns=['y', 'w', 'u', 'v', 'm', 'd_osm', 'd_iudm', 'h', 'f'])
     for y in flood_ser.index:
         w = flood_ser[y]
         f = judge_damage(w, h)
         state_iudm = [f, u, v, d_iudm]
         state_osm = [f, m, d_osm]
-        u, v, d_iudm = iudm(state_iudm, steps, k_iudm)
-        m, d_osm = osm(state_osm, steps, k_osm)
+        u, v, d_iudm = iudm(state_iudm, steps, k_iudm, pop_growth)
+        m, d_osm = osm(state_osm, steps, k_osm, pop_growth)
         state_all = np.array([y, w, u, v, m, d_osm, d_iudm, h, f])
         h = get_levee_height(state_all, how)  # Levee height in the next year
         df.loc[y, :] = state_all
+    df['loss_osm'] = df['f'] * df['d_osm']
+    df['loss_iudm'] = df['f'] * df['d_iudm']
+    df['u+v'] = df['u'] + df['v']
     return df
 
 
